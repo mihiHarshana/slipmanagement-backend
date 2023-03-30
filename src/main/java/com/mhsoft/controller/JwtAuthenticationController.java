76ac0c06faf729dd91406d3bac551ae8a6db21f5
mihiHarshana <mihinduwijesena@gmail.com>
2023-03-30 08:12:32 +0530

    Handled invalid credentials

--- src/main/java/com/mhsoft/controller/JwtAuthenticationController.java
+++ src/main/java/com/mhsoft/controller/JwtAuthenticationController.java
@@ -4,15 +4,19 @@ import java.util.Objects;
 
 import javax.print.attribute.standard.DateTimeAtCompleted;
 
+import com.mhsoft.utils.Utils;
+import com.mysql.cj.util.Util;
 import org.apache.tomcat.jni.Time;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.ui.context.support.UiApplicationContextUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -57,20 +61,25 @@ public class JwtAuthenticationController {
 	
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
 	public String createAuthenticationToken(@RequestBody JwtRequest authenticationRequest) throws Exception {
+		try {
+			authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
 
-		authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
+			final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
 
-		final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+			final String token = jwtTokenUtil.generateToken(userDetails);
 
-		final String token = jwtTokenUtil.generateToken(userDetails);
-		
-		
-		JSONObject jo = new JSONObject();
-		jo.put("token", token);
-		jo.put("mobile", userDetails.getUsername());
-		jo.put("logintime", System.currentTimeMillis());
 
-		return jo.toString();
+			JSONObject jo = new JSONObject();
+			jo.put("token", token);
+			jo.put("mobile", userDetails.getUsername());
+			jo.put("logintime", System.currentTimeMillis());
+
+			return jo.toString();
+		} catch (Exception e){
+			Utils utils = new Utils();
+			return  utils.JsonMessage("Invalid Credentails", HttpStatus.ACCEPTED);
+		}
+
 	}
 
 	/*
