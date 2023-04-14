4d2d33a6d3768ad9723ea307902c9ab86abe032b
mihiHarshana <mihinduwijesena@gmail.com>
2023-04-14 16:00:28 +0530

    correctd static usage

--- src/main/java/com/mhsoft/controller/UserDetailsController.java
+++ src/main/java/com/mhsoft/controller/UserDetailsController.java
@@ -33,17 +33,17 @@ public class UserDetailsController {
         Utils utils = Utils.getInstance();
 
         if (bankDetailsOfUser == null) {
-            userBankDetails.put(utils.BANK_NAME, "null");
-            userBankDetails.put(utils.BANK_CODE, "null");
-            userBankDetails.put(utils.BANK_BRANCH, "null");
-            userBankDetails.put(utils.BANK_INS, "null");
-            userBankDetails.put(utils.BANK_ACC_NO, "null");
+            userBankDetails.put(Utils.BANK_NAME, "null");
+            userBankDetails.put(Utils.BANK_CODE, "null");
+            userBankDetails.put(Utils.BANK_BRANCH, "null");
+            userBankDetails.put(Utils.BANK_INS, "null");
+            userBankDetails.put(Utils.BANK_ACC_NO, "null");
         }
-        userBankDetails.put(utils.BANK_NAME, bankDetailsOfUser.getBankname());
-        userBankDetails.put(utils.BANK_CODE, bankDetailsOfUser.getBankcode());
-        userBankDetails.put(utils.BANK_BRANCH, bankDetailsOfUser.getBranchname());
-        userBankDetails.put(utils.BANK_INS, bankDetailsOfUser.getBankinst());
-        userBankDetails.put(utils.BANK_ACC_NO, bankDetailsOfUser.getBankaccno());
+        userBankDetails.put(Utils.BANK_NAME, bankDetailsOfUser.getBankname());
+        userBankDetails.put(Utils.BANK_CODE, bankDetailsOfUser.getBankcode());
+        userBankDetails.put(Utils.BANK_BRANCH, bankDetailsOfUser.getBranchname());
+        userBankDetails.put(Utils.BANK_INS, bankDetailsOfUser.getBankinst());
+        userBankDetails.put(Utils.BANK_ACC_NO, bankDetailsOfUser.getBankaccno());
 
         DAOTransaction [] userTrDetails =   trService.getTransactionsByUserId(userDetails.getUserid());
         String [] tempAgentDetails = userDetailService.getAgentDetailsByUserId(userDetails.getUserid());
@@ -51,17 +51,17 @@ public class UserDetailsController {
         JSONObject jsonAgentDetalils = new JSONObject();
 
         if (tempAgentDetails == null) {
-            jsonAgentDetalils.put(utils.concatinateAgent(utils.BANK_ACC_NO),"null");
-            jsonAgentDetalils.put(utils.concatinateAgent(utils.BANK_CODE),"null");
-            jsonAgentDetalils.put(utils.concatinateAgent(utils.BANK_INS),"null");
-            jsonAgentDetalils.put(utils.concatinateAgent(utils.BANK_NAME),"null");
-            jsonAgentDetalils.put(utils.concatinateAgent(utils.BANK_BRANCH),"null");
+            jsonAgentDetalils.put(utils.concatinateAgent(Utils.BANK_ACC_NO),"null");
+            jsonAgentDetalils.put(utils.concatinateAgent(Utils.BANK_CODE),"null");
+            jsonAgentDetalils.put(utils.concatinateAgent(Utils.BANK_INS),"null");
+            jsonAgentDetalils.put(utils.concatinateAgent(Utils.BANK_NAME),"null");
+            jsonAgentDetalils.put(utils.concatinateAgent(Utils.BANK_BRANCH),"null");
         } else {
-            jsonAgentDetalils.put(utils.concatinateAgent(utils.BANK_ACC_NO),tempAgentDetails[0]);
-            jsonAgentDetalils.put(utils.concatinateAgent(utils.BANK_CODE),tempAgentDetails[1]);
-            jsonAgentDetalils.put(utils.concatinateAgent(utils.BANK_INS),tempAgentDetails[2]);
-            jsonAgentDetalils.put(utils.concatinateAgent(utils.BANK_NAME),tempAgentDetails[3]);
-            jsonAgentDetalils.put(utils.concatinateAgent(utils.BANK_BRANCH),tempAgentDetails[4]);
+            jsonAgentDetalils.put(utils.concatinateAgent(Utils.BANK_ACC_NO),tempAgentDetails[0]);
+            jsonAgentDetalils.put(utils.concatinateAgent(Utils.BANK_CODE),tempAgentDetails[1]);
+            jsonAgentDetalils.put(utils.concatinateAgent(Utils.BANK_INS),tempAgentDetails[2]);
+            jsonAgentDetalils.put(utils.concatinateAgent(Utils.BANK_NAME),tempAgentDetails[3]);
+            jsonAgentDetalils.put(utils.concatinateAgent(Utils.BANK_BRANCH),tempAgentDetails[4]);
         }
         JSONObject allDetails = new JSONObject();
         allDetails.put("userBankDetails",userBankDetails );
