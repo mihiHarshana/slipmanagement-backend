32ebaeb447359e0b4c8d20438c52cfbc70f71320
mihiHarshana <mihinduwijesena@gmail.com>
2023-04-26 08:08:20 +0530

    Remove unused imported

--- src/main/java/com/mhsoft/controller/UserDetailsController.java
+++ src/main/java/com/mhsoft/controller/UserDetailsController.java
@@ -15,10 +15,7 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 
 @RestController
 @CrossOrigin
